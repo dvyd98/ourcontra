dc61d95782e660cfb9ac17765c7947a4c70401c4
Dvyd <dvyd98@gmail.com>
2019-09-28 20:48:30 +0200

    Now Player can aim in all directions and walk while aiming. Added placeholders for such animations

--- 02-Bubble/Player.cpp
+++ 02-Bubble/Player.cpp
@@ -13,16 +13,16 @@
 
 enum PlayerAnims
 {
-	STAND_LEFT, STAND_RIGHT, MOVE_LEFT, MOVE_RIGHT
+	STAND_LEFT, STAND_RIGHT, MOVE_LEFT, MOVE_RIGHT, AIM_UP_LOOK_LEFT, AIM_UP_LOOK_RIGHT, CROUCH_LOOK_LEFT, CROUCH_LOOK_RIGHT, AIM_DOWN, AIM_UP_WALK_RIGHT, AIM_UP_WALK_LEFT, AIM_DOWN_WALK_RIGHT, AIM_DOWN_WALK_LEFT
 };
 
 
 void Player::init(const glm::ivec2 &tileMapPos, ShaderProgram &shaderProgram)
 {
 	bJumping = false;
-	spritesheet.loadFromFile("images/bub.png", TEXTURE_PIXEL_FORMAT_RGBA);
+	spritesheet.loadFromFile("images/guyPH.png", TEXTURE_PIXEL_FORMAT_RGBA);
 	sprite = Sprite::createSprite(glm::ivec2(32, 32), glm::vec2(0.25, 0.25), &spritesheet, &shaderProgram);
-	sprite->setNumberAnimations(4);
+	sprite->setNumberAnimations(13);
 	
 		sprite->setAnimationSpeed(STAND_LEFT, 8);
 		sprite->addKeyframe(STAND_LEFT, glm::vec2(0.f, 0.f));
@@ -32,13 +32,33 @@ void Player::init(const glm::ivec2 &tileMapPos, ShaderProgram &shaderProgram)
 		
 		sprite->setAnimationSpeed(MOVE_LEFT, 8);
 		sprite->addKeyframe(MOVE_LEFT, glm::vec2(0.f, 0.f));
-		sprite->addKeyframe(MOVE_LEFT, glm::vec2(0.f, 0.25f));
-		sprite->addKeyframe(MOVE_LEFT, glm::vec2(0.f, 0.5f));
 		
 		sprite->setAnimationSpeed(MOVE_RIGHT, 8);
 		sprite->addKeyframe(MOVE_RIGHT, glm::vec2(0.25, 0.f));
-		sprite->addKeyframe(MOVE_RIGHT, glm::vec2(0.25, 0.25f));
-		sprite->addKeyframe(MOVE_RIGHT, glm::vec2(0.25, 0.5f));
+
+		sprite->setAnimationSpeed(AIM_UP_LOOK_LEFT, 8);
+		sprite->addKeyframe(AIM_UP_LOOK_LEFT, glm::vec2(0.75f, 0.50f));
+
+		sprite->setAnimationSpeed(AIM_UP_LOOK_RIGHT, 8);
+		sprite->addKeyframe(AIM_UP_LOOK_RIGHT, glm::vec2(0.50f, 0.50f));
+
+		sprite->setAnimationSpeed(AIM_UP_WALK_RIGHT, 8);
+		sprite->addKeyframe(AIM_UP_WALK_RIGHT, glm::vec2(0.75f, 0.75f));
+
+		sprite->setAnimationSpeed(AIM_UP_WALK_LEFT, 8);
+		sprite->addKeyframe(AIM_UP_WALK_LEFT, glm::vec2(0.50f, 0.75f));
+
+		sprite->setAnimationSpeed(CROUCH_LOOK_LEFT, 8);
+		sprite->addKeyframe(CROUCH_LOOK_LEFT, glm::vec2(0.0f, 0.75f));
+
+		sprite->setAnimationSpeed(CROUCH_LOOK_RIGHT, 8);
+		sprite->addKeyframe(CROUCH_LOOK_RIGHT, glm::vec2(0.25f, 0.75f));
+
+		sprite->setAnimationSpeed(AIM_DOWN_WALK_RIGHT, 8);
+		sprite->addKeyframe(AIM_DOWN_WALK_RIGHT, glm::vec2(0.25f, 0.50f));
+
+		sprite->setAnimationSpeed(AIM_DOWN_WALK_LEFT, 8);
+		sprite->addKeyframe(AIM_DOWN_WALK_LEFT, glm::vec2(0.0f, 0.50f));
 		
 	sprite->changeAnimation(0);
 	tileMapDispl = tileMapPos;
@@ -49,7 +69,51 @@ void Player::init(const glm::ivec2 &tileMapPos, ShaderProgram &shaderProgram)
 void Player::update(int deltaTime)
 {
 	sprite->update(deltaTime);
-	if(Game::instance().getSpecialKey(GLUT_KEY_LEFT))
+	if (Game::instance().getSpecialKey(GLUT_KEY_LEFT) && Game::instance().getSpecialKey(GLUT_KEY_UP))
+	{
+		if (sprite->animation() != AIM_UP_WALK_LEFT)
+			sprite->changeAnimation(AIM_UP_WALK_LEFT);
+		posPlayer.x -= 2;
+		if (map->collisionMoveLeft(posPlayer, glm::ivec2(32, 32)))
+		{
+			posPlayer.x += 2;
+			sprite->changeAnimation(STAND_LEFT);
+		}
+	}
+	else if (Game::instance().getSpecialKey(GLUT_KEY_RIGHT) && Game::instance().getSpecialKey(GLUT_KEY_UP))
+	{
+		if (sprite->animation() != AIM_UP_WALK_RIGHT)
+		sprite->changeAnimation(AIM_UP_WALK_RIGHT);
+		posPlayer.x += 2;
+		if (map->collisionMoveLeft(posPlayer, glm::ivec2(32, 32)))
+		{
+		posPlayer.x -= 2;
+		sprite->changeAnimation(STAND_RIGHT);
+		}
+	}
+	else if (Game::instance().getSpecialKey(GLUT_KEY_LEFT) && Game::instance().getSpecialKey(GLUT_KEY_DOWN))
+	{
+		if (sprite->animation() != AIM_DOWN_WALK_LEFT)
+			sprite->changeAnimation(AIM_DOWN_WALK_LEFT);
+		posPlayer.x -= 2;
+		if (map->collisionMoveLeft(posPlayer, glm::ivec2(32, 32)))
+		{
+			posPlayer.x += 2;
+			sprite->changeAnimation(STAND_LEFT);
+		}
+	}
+	else if (Game::instance().getSpecialKey(GLUT_KEY_RIGHT) && Game::instance().getSpecialKey(GLUT_KEY_DOWN))
+	{
+		if (sprite->animation() != AIM_DOWN_WALK_RIGHT)
+			sprite->changeAnimation(AIM_DOWN_WALK_RIGHT);
+		posPlayer.x += 2;
+		if (map->collisionMoveLeft(posPlayer, glm::ivec2(32, 32)))
+		{
+			posPlayer.x -= 2;
+			sprite->changeAnimation(STAND_RIGHT);
+		}
+	}
+	else if(Game::instance().getSpecialKey(GLUT_KEY_LEFT))
 	{
 		if(sprite->animation() != MOVE_LEFT)
 			sprite->changeAnimation(MOVE_LEFT);
@@ -71,12 +135,43 @@ void Player::update(int deltaTime)
 			sprite->changeAnimation(STAND_RIGHT);
 		}
 	}
+	else if (Game::instance().getSpecialKey(GLUT_KEY_UP))
+	{
+		if (sprite->animation() == STAND_LEFT || sprite->animation() == AIM_UP_WALK_LEFT)
+		sprite->changeAnimation(AIM_UP_LOOK_LEFT);
+		else if (sprite->animation() == STAND_RIGHT || sprite->animation() == AIM_UP_WALK_RIGHT)
+			sprite->changeAnimation(AIM_UP_LOOK_RIGHT);
+	}
+	else if (Game::instance().getSpecialKey(GLUT_KEY_DOWN))
+	{
+		if (sprite->animation() == STAND_LEFT || sprite->animation() == AIM_DOWN_WALK_LEFT)
+			sprite->changeAnimation(CROUCH_LOOK_LEFT);
+		else if (sprite->animation() == STAND_RIGHT || sprite->animation() == AIM_DOWN_WALK_RIGHT)
+			sprite->changeAnimation(CROUCH_LOOK_RIGHT);
+	}
 	else
 	{
 		if(sprite->animation() == MOVE_LEFT)
 			sprite->changeAnimation(STAND_LEFT);
 		else if(sprite->animation() == MOVE_RIGHT)
 			sprite->changeAnimation(STAND_RIGHT);
+		else if (sprite->animation() == AIM_UP_LOOK_LEFT)
+			sprite->changeAnimation(STAND_LEFT);
+		else if (sprite->animation() == AIM_UP_LOOK_RIGHT)
+			sprite->changeAnimation(STAND_RIGHT);
+		else if (sprite->animation() == AIM_UP_WALK_LEFT)
+			sprite->changeAnimation(STAND_LEFT);
+		else if (sprite->animation() == AIM_UP_WALK_RIGHT)
+			sprite->changeAnimation(STAND_RIGHT);
+		else if (sprite->animation() == CROUCH_LOOK_LEFT)
+			sprite->changeAnimation(STAND_LEFT);
+		else if (sprite->animation() == CROUCH_LOOK_RIGHT)
+			sprite->changeAnimation(STAND_RIGHT);
+		else if (sprite->animation() == AIM_DOWN_WALK_LEFT)
+			sprite->changeAnimation(STAND_LEFT);
+		else if (sprite->animation() == AIM_DOWN_WALK_RIGHT)
+			sprite->changeAnimation(STAND_RIGHT);
+
 	}
 	
 	if(bJumping)
@@ -99,7 +194,7 @@ void Player::update(int deltaTime)
 		posPlayer.y += FALL_STEP;
 		if(map->collisionMoveDown(posPlayer, glm::ivec2(32, 32), &posPlayer.y))
 		{
-			if(Game::instance().getSpecialKey(GLUT_KEY_UP))
+			if(Game::instance().getKey(GLUT_KEY_SPACEBAR))
 			{
 				bJumping = true;
 				jumpAngle = 0;
