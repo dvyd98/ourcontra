1227e1bee3913788b3e472b0c900d17c93af1c7e
Paula Zhao <luhari1998@gmail.com>
2019-09-29 17:18:15 +0200

    TileMap Workiiiiingggg!!!!

--- 02-Bubble/TileMap.cpp
+++ 02-Bubble/TileMap.cpp
@@ -8,7 +8,13 @@
 using namespace std;
 
 enum BACKGROUND_TILES {
-
+	BLACK, GRASS1, GRASS2, ROCK1, ROCK2, BRIDGE1, BRIDGE2, BLANK1, PALM_TOP1, PALM_TOP2, BLANK2, PALM_TOP3, BLANK3, PALM_TOP4, STAR1, STAR2,
+	BLUE, ROCK3, ROCK4, ROCK5, ROCK6, BRIDGE3, BRIDGE4, BRIDGE5, PALM_BOT1, PALM_BOT2, PALM_BOT3, PALM_BOT4, PALM_BOT5, PALM_BOT6, START3, STAR4,
+	BLANK4, ROCK7, ROCK8, SEA1, SEA2, SEA3, SEA4, SEA5, SEA6, SEA7, SEA8, SEA9, BLANK5, BLANK6,
+	JUNGLE1, JUNGLE2, BLANK7, JUNGLE3, BLANK8, BLANK9, BLANK10, BLANK11, M1, M2, M3, M4, BLANK12, BLANK13, BLANK14, BLANK15,
+	JUNGLE4, JUNGLE5, BLANK16, JUGNLE6, JUNGLE7, BLANK17, BLANK18, M5, M6, M7, M8, M9, M10, BLANK19, BLANK20, BLANK21,
+	JUNLE8, JUNGLE10, BLANK22, BLANK23, BLANK24, BLANK25, BLANK26, M11, M12, M13, M14, BLANK27, M15, BLANK28, BLANK, BLANK29,
+	JUNGLE11, JUNGLE12
 };
 
 
@@ -87,13 +93,22 @@ bool TileMap::loadLevel(const string &levelFile)
 		for(int i=0; i<mapSize.x; i++)
 		{
 			fin.get(tile);
-			/* switch (tile) {
-				case ' ': map[j*mapSize.x + i] = 0; break;
-			}*/
-			if (tile == '.')
-				map[j*mapSize.x + i] = 0;
-			else
-				map[j*mapSize.x+i] = tile - int('0');
+			switch (tile) {
+				case '.': map[j*mapSize.x + i] = BLACK; break;
+				case ',': map[j*mapSize.x + i] = BLUE; break;
+				case 'q': map[j*mapSize.x + i] = GRASS1; break;
+				case 'w': map[j*mapSize.x + i] = GRASS2; break;
+				case 'a': map[j*mapSize.x + i] = ROCK3; break;
+				case 's': map[j*mapSize.x + i] = ROCK4; break;
+				case 'e': map[j*mapSize.x + i] = ROCK1; break;
+				case 'r': map[j*mapSize.x + i] = ROCK2; break;
+				case 'd': map[j*mapSize.x + i] = ROCK5; break;
+				case 'f': map[j*mapSize.x + i] = ROCK6; break;
+				case 'z': map[j*mapSize.x + i] = ROCK7; break;
+				case 'x': map[j*mapSize.x + i] = ROCK8; break;
+
+
+			}
 		}
 		fin.get(tile);
 #ifndef _WIN32
@@ -117,30 +132,28 @@ void TileMap::prepareArrays(const glm::vec2 &minCoords, ShaderProgram &program)
 		for(int i=0; i<mapSize.x; i++)
 		{
 			tile = map[j * mapSize.x + i];
-			if(tile != 0)
-			{
-				// Non-empty tile
-				nTiles++;
-				posTile = glm::vec2(minCoords.x + i * tileSize, minCoords.y + j * tileSize);
-				texCoordTile[0] = glm::vec2(float((tile-1)%2) / tilesheetSize.x, float((tile-1)/2) / tilesheetSize.y);
-				texCoordTile[1] = texCoordTile[0] + tileTexSize;
-				//texCoordTile[0] += halfTexel;
-				texCoordTile[1] -= halfTexel;
-				// First triangle
-				vertices.push_back(posTile.x); vertices.push_back(posTile.y);
-				vertices.push_back(texCoordTile[0].x); vertices.push_back(texCoordTile[0].y);
-				vertices.push_back(posTile.x + blockSize); vertices.push_back(posTile.y);
-				vertices.push_back(texCoordTile[1].x); vertices.push_back(texCoordTile[0].y);
-				vertices.push_back(posTile.x + blockSize); vertices.push_back(posTile.y + blockSize);
-				vertices.push_back(texCoordTile[1].x); vertices.push_back(texCoordTile[1].y);
-				// Second triangle
-				vertices.push_back(posTile.x); vertices.push_back(posTile.y);
-				vertices.push_back(texCoordTile[0].x); vertices.push_back(texCoordTile[0].y);
-				vertices.push_back(posTile.x + blockSize); vertices.push_back(posTile.y + blockSize);
-				vertices.push_back(texCoordTile[1].x); vertices.push_back(texCoordTile[1].y);
-				vertices.push_back(posTile.x); vertices.push_back(posTile.y + blockSize);
-				vertices.push_back(texCoordTile[0].x); vertices.push_back(texCoordTile[1].y);
-			}
+
+			// Non-empty tile
+			nTiles++;
+			posTile = glm::vec2(minCoords.x + i * tileSize, minCoords.y + j * tileSize);
+			texCoordTile[0] = glm::vec2(float(tile%16) / tilesheetSize.x, float(tile/16) / tilesheetSize.y);
+			texCoordTile[1] = texCoordTile[0] + tileTexSize;
+			//texCoordTile[0] += halfTexel;
+			texCoordTile[1] -= halfTexel;
+			// First triangle
+			vertices.push_back(posTile.x); vertices.push_back(posTile.y);
+			vertices.push_back(texCoordTile[0].x); vertices.push_back(texCoordTile[0].y);
+			vertices.push_back(posTile.x + blockSize); vertices.push_back(posTile.y);
+			vertices.push_back(texCoordTile[1].x); vertices.push_back(texCoordTile[0].y);
+			vertices.push_back(posTile.x + blockSize); vertices.push_back(posTile.y + blockSize);
+			vertices.push_back(texCoordTile[1].x); vertices.push_back(texCoordTile[1].y);
+			// Second triangle
+			vertices.push_back(posTile.x); vertices.push_back(posTile.y);
+			vertices.push_back(texCoordTile[0].x); vertices.push_back(texCoordTile[0].y);
+			vertices.push_back(posTile.x + blockSize); vertices.push_back(posTile.y + blockSize);
+			vertices.push_back(texCoordTile[1].x); vertices.push_back(texCoordTile[1].y);
+			vertices.push_back(posTile.x); vertices.push_back(posTile.y + blockSize);
+			vertices.push_back(texCoordTile[0].x); vertices.push_back(texCoordTile[1].y);
 		}
 	}
 
