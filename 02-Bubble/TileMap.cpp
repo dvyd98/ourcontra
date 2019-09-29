bd61228a5b1850cd0f1511d2b177b95faecf3308
Paula Zhao <luhari1998@gmail.com>
2019-09-29 19:10:07 +0200

    Millors colisions

--- 02-Bubble/TileMap.cpp
+++ 02-Bubble/TileMap.cpp
@@ -179,8 +179,8 @@ bool TileMap::collisionMoveLeft(const glm::ivec2 &pos, const glm::ivec2 &size) c
 	y1 = (pos.y + size.y - 1) / tileSize;
 	for(int y=y0; y<=y1; y++)
 	{
-		if(map[y*mapSize.x+x] != 0)
-			return true;
+		/*if(map[y*mapSize.x+x] != 0)
+			return true;*/
 	}
 	
 	return false;
@@ -195,8 +195,8 @@ bool TileMap::collisionMoveRight(const glm::ivec2 &pos, const glm::ivec2 &size) 
 	y1 = (pos.y + size.y - 1) / tileSize;
 	for(int y=y0; y<=y1; y++)
 	{
-		if(map[y*mapSize.x+x] != 0)
-			return true;
+		/*if(map[y*mapSize.x+x] != 0)
+			return true;*/
 	}
 	
 	return false;
@@ -211,7 +211,7 @@ bool TileMap::collisionMoveDown(const glm::ivec2 &pos, const glm::ivec2 &size, i
 	y = (pos.y + size.y - 1) / tileSize;
 	for(int x=x0; x<=x1; x++)
 	{
-		if(map[y*mapSize.x+x] != 0)
+		if(map[y*mapSize.x + x] == GRASS1 || map[y*mapSize.x + x] == GRASS2)
 		{
 			if(*posY - tileSize * y + size.y <= 4)
 			{
